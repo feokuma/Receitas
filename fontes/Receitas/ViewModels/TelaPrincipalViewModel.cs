1ec163f27747927dec4778b4dbdc044cf2d61660
Fernando Okuma <feokuma@gmail.com>
2020-08-02 22:20:34 -0300

    Tela principal do app

--- fontes/Receitas/ViewModels/TelaPrincipalViewModel.cs
+++ fontes/Receitas/ViewModels/TelaPrincipalViewModel.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+
 namespace Receitas.ViewModels
 {
-    public class TelaPrincipalViewModel
+    public class TelaPrincipalViewModel : BindableBase
     {
+        private string _TextoPrincipal = "Texto da tela principal";
+        public string TextoPrincipal
+        {
+            get { return _TextoPrincipal; }
+            set { SetProperty(ref _TextoPrincipal, value); }
+        }
+
+        private bool _EstáSelecionado;
+        public bool EstáSelecionado
+        {
+            get { return _EstáSelecionado; } 
+            set { SetProperty(ref _EstáSelecionado, value); } 
+        }
+
+        public DelegateCommand BotãoCommand { get; set; }
+
         public TelaPrincipalViewModel()
         {
+            BotãoCommand = new DelegateCommand(BotãoAcionado)
+                .ObservesCanExecute(() => EstáSelecionado);
+        }
+
+        private void BotãoAcionado()
+        {
+            TextoPrincipal = "Botão acionado";
         }
     }
 }
