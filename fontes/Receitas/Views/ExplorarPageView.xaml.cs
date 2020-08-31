64106e2536ce4d924fb833c6c1dc786cf0c5e939
Fernando Okuma <feokuma@gmail.com>
2020-08-31 00:41:31 -0300

    Implementa transição entre as trocas de tabs

--- fontes/Receitas/Views/ExplorarPageView.xaml.cs
+++ fontes/Receitas/Views/ExplorarPageView.xaml.cs
@@ -13,9 +13,36 @@ namespace Receitas.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ExplorarPageView : ContentPage
     {
+        private int TabSelecionada = 0;
+        private List<Label> TabLabels = new List<Label>();
+
         public ExplorarPageView()
         {
             InitializeComponent();
+
+            TabLabels.Add(labelTabExplorar);
+            TabLabels.Add(labelTabCozinharAgora);
+            TabLabels.Add(labelTabFavoritos);
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var label = sender as Label;
+            var index = TabLabels.IndexOf(label);
+
+            await ExibirConteúdoTabSelecionada(index);
+        }
+
+        private async Task ExibirConteúdoTabSelecionada(int index)
+        {
+            if (index == TabSelecionada)
+                return;
+
+            TabLabels[TabSelecionada].FontAttributes = FontAttributes.None;
+            var tabLabelSelecionada = TabLabels[index];
+            _ = await marcadorSeleção.TranslateTo(tabLabelSelecionada.X, 0, 100, Easing.SinInOut);
+            tabLabelSelecionada.FontAttributes = FontAttributes.Bold;
+            TabSelecionada = index;
         }
     }
 }
